23a97e651a221aebc4909aa7e6547b70ebd540e5
husain <hi.ansari11@gmail.com>
2017-08-04 07:08:45 +0530

    not changed

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-ui-grid/ui-grid.js',
       'bower_components/moment/moment.js',
+      'bower_components/angular-toastr/dist/angular-toastr.tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
