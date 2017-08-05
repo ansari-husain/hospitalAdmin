6e9c48586588967ab6fc2b8463db440f43656c72
husain <hi.ansari11@gmail.com>
2017-08-05 17:01:19 +0530

    applied user authentication to check username or password is correct or not and added error message for invalid user

--- app/scripts/login.controller.js
+++ app/scripts/login.controller.js
@@ -1,14 +1,19 @@
 'use strict';
 
 angular.module('hospitaladminApp')
-	.controller('LoginCtrl', function ($rootScope, $location, loginService, $cookies, cookieName, Auth) {
+	.controller('LoginCtrl', function ($rootScope, $location, loginService, $cookies, cookieName, Auth, toastr) {
 		var vm = this;
 
 		vm.fnLogin = function () {
 			Auth.login(vm.user)
 				.then(function (res) {
-						$location.path('/patient-data');
 
+            if (res && res.data && res.data.userName) {
+              toastr.success( res.data.userName[0].username, 'Welcome ');
+              $location.path('/patient-data');
+            }else {
+              toastr.error('Authentication Failed');
+            }
 					//console.log('hello'+JSON.stringify(res.data.userName[0]));
 				}, function (err) {
 					vm.user = {};
